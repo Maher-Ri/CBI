02056630de3d350f18eae94bf89d1412cceced02
Maher-Al-Rifai <maher.refae15@gmail.com>
2025-11-18 03:04:48 +0200

    create footer and clients sections

--- src/components/home/Swiper.jsx
+++ src/components/home/Swiper.jsx
@@ -1,12 +1,12 @@
 import React, { useRef } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation } from "swiper/modules";
+import { Autoplay, Navigation } from "swiper/modules";
 
 import "swiper/css"; // ONLY this one!
 
 import img from "../../assets/slide1.png";
 import arrowLeft from "../../assets/arrow-left-blue.svg";
-import arrowRight from "../../assets/aroow-right-blue.svg";
+import arrowRight from "../../assets/arrow-right-blue.svg";
 export default function CustomSwiper() {
   const prevRef = useRef(null);
   const nextRef = useRef(null);
@@ -16,30 +16,26 @@ export default function CustomSwiper() {
       {/* Custom Prev Button */}
       <button
         ref={prevRef}
-        className="absolute  left-[5%] top-[90%] md:top-1/2 -translate-y-1/2 z-20 flex items-center justify-center border-[2px] border-mainBlue rounded-lg p-3 md:p-5 shadow bg-gradient-to-tr from-white/0 to-white/0
-     hover:bg-gradient-to-tr hover:from-mainBlue/80 hover:via-gray hover:to-[#555]
-    transition-all duration-300 "
+        className="absolute overflow-hidden left-[5%] top-[90%] md:top-1/2 -translate-y-1/2 z-20 flex items-center justify-center border-[2px] border-mainBlue rounded-lg p-3 md:p-5 my-gradient-box2"
       >
-        <img src={arrowLeft} alt="<" className="w-[12px] md:w-[16px]"/>
+        <img src={arrowLeft} alt="<" className="w-[12px] md:w-[16px]" />
       </button>
 
       {/* Custom Next Button */}
       <button
         ref={nextRef}
-        className="absolute right-[5%] top-[90%] md:top-1/2 -translate-y-1/2 z-20 flex items-center justify-center border-[2px] border-mainBlue rounded-lg p-3 md:p-5 shadow bg-gradient-to-tr from-white/0 to-white/0
-    hover:bg-gradient-to-tr hover:from-mainBlue/80 hover:via-[gray] hover:to-[#555]
-    transition-all duration-300"
+        className="absolute overflow-hidden right-[5%] top-[90%] md:top-1/2 -translate-y-1/2 z-20 flex items-center justify-center border-[2px] border-mainBlue rounded-lg p-3 md:p-5 my-gradient-box2"
       >
-        <img src={arrowRight} alt=">" className="w-[12px] md:w-[16px]"/>
+        <img src={arrowRight} alt=">" className="w-[12px] md:w-[16px]" />
       </button>
 
       <Swiper
-        modules={[Navigation]}
+        modules={[Autoplay, Navigation]}
         navigation={false}
         loop={true}
         autoplay={{
-          delay: 3000,
-          disableOnInteraction: true,
+          delay: 2000,
+          disableOnInteraction: false,
         }}
         onSwiper={(swiper) => {
           // Connect custom buttons
@@ -52,8 +48,12 @@ export default function CustomSwiper() {
         slidesPerView={1}
       >
         <SwiperSlide className="h-full">
-          <div className="relative w-full h-full text-left">
-            <img src={img} alt="Slide" className=" h-[400px] md:w-full md:h-auto  object-none md:object-cover" />
+          <div className="relative w-full h-full text-left ">
+            <img
+              src={img}
+              alt="Slide"
+              className=" h-[400px] md:w-full md:h-auto  object-none md:object-cover"
+            />
 
             <div className="absolute inset-0 flex flex-col items-start justify-center text-mainBlue gap-8 px-[15%]">
               <h2 className=" text-xl md:text-2xl lg:text-[57px] lg:leading-[70px] font-extrabold">
@@ -70,7 +70,11 @@ export default function CustomSwiper() {
         </SwiperSlide>
         <SwiperSlide className="h-full">
           <div className="relative w-full h-full text-left">
-            <img src={img} alt="Slide" className=" h-[400px] md:w-full md:h-auto  object-none md:object-cover" />
+            <img
+              src={img}
+              alt="Slide"
+              className=" h-[400px] md:w-full md:h-auto  object-none md:object-cover"
+            />
 
             <div className="absolute inset-0 flex flex-col items-start justify-center text-mainBlue gap-8 px-[15%]">
               <h2 className=" text-xl md:text-2xl lg:text-[57px] lg:leading-[70px] font-extrabold">
@@ -87,7 +91,11 @@ export default function CustomSwiper() {
         </SwiperSlide>
         <SwiperSlide className="h-full">
           <div className="relative w-full h-full text-left">
-            <img src={img} alt="Slide" className=" h-[400px] md:w-full md:h-auto  object-none md:object-cover" />
+            <img
+              src={img}
+              alt="Slide"
+              className=" h-[400px] md:w-full md:h-auto  object-none md:object-cover"
+            />
 
             <div className="absolute inset-0 flex flex-col items-start justify-center text-mainBlue gap-8 px-[15%]">
               <h2 className=" text-xl md:text-2xl lg:text-[57px] lg:leading-[70px] font-extrabold">
--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,13 +1,15 @@
+import logo from "../../assets/Logo.svg";
 export default function Footer() {
   return (
-    <footer className="bg-mainBlue text-white pt-16 pb-8 px-6 md:px-12 lg:px-24">
+    <footer className="bg-mainBlue text-white pt-16 pb-8 ">
       {/* TOP SECTIONS */}
-      <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-3 gap-12 text-left">
-
+      <div className="w-full pl-[5%] lg:pl-[15%] pr-[5%] grid grid-cols-1 md:grid-cols-3 gap-12 text-left">
         {/* ABOUT CBI */}
         <div>
-          <h3 classname=" text-xl font-bold mb-6">ABOUT CBI</h3>
-          <ul className="space-y-2 text-sm">
+          <h3 className=" text-xl xl:text-3xl font-extrabold mb-6">
+            ABOUT CBI
+          </h3>
+          <ul className="space-y-2 text-[17px] font-medium uppercase">
             <li>ACCUEIL</li>
             <li>À PROPOS</li>
             <li>SERVICES</li>
@@ -18,10 +20,13 @@ export default function Footer() {
 
         {/* INFO */}
         <div>
-          <h3 className="text-xl font-bold mb-6">INFO</h3>
-          <ul className="space-y-2 text-sm">
-            <li>30 Division, New York</li>
-            <li>NY 1000, USA</li>
+          <h3 className="text-xl xl:text-3xl font-extrabold mb-6">INFO</h3>
+          <ul className="space-y-6 text-[17px] font-medium">
+            <li>
+              30 Division, New York
+              <br />
+              NY 1000, USA
+            </li>
             <li className="mt-4">+1 600 123 456 789</li>
             <li>info@cbi.com</li>
           </ul>
@@ -29,20 +34,25 @@ export default function Footer() {
 
         {/* STAY IN TOUCH */}
         <div>
-          <h3 className="text-xl font-bold mb-6">STAY IN TOUCH</h3>
-          <p className="text-sm mb-4">Get News & Updates</p>
+          <h3 className="text-xl xl:text-3xl font-extrabold mb-6">
+            STAY IN TOUCH
+          </h3>
 
           {/* Email Input */}
-          <div className="flex items-center border-b border-gray-400 pb-2 mb-6">
+          <div className="flex items-center border-b border-gray-400 pb-6 mb-2">
             <input
               type="email"
-              placeholder="Share your email to get updates…"
-              className="bg-transparent outline-none text-sm flex-1 placeholder-gray-300"
+              placeholder="Get News & Updates"
+              className="bg-transparent outline-none text-[17px] flex-1 placeholder-white placeholder:text-[17px] font-medium placeholder:font-medium"
             />
-            <button className="text-gray-300 hover:text-white transition">➤</button>
+            <button className="text-gray-300 hover:text-white transition text-[17px] font-medium">
+              @
+            </button>
           </div>
-
-          <ul className="space-y-2 text-sm">
+          <span className="leading-none text-[17px] font-medium text-white/20 ">
+            Share your email to get all new and<br className="hidden xl:block"/>updates related to CBI
+          </span>
+          <ul className="space-y-2 text-[17px] font-medium mt-4">
             <li>CONTACT US</li>
             <li>TERMS & CONDITIONS</li>
           </ul>
@@ -50,14 +60,12 @@ export default function Footer() {
       </div>
 
       {/* DIVIDER */}
-      <div className="border-t border-gray-500 my-12 opacity-40"></div>
+      <div className="border-t-2 border-white my-12 opacity-40 mx-[5%]"></div>
 
       {/* BOTTOM COPYRIGHT */}
-      <div className="max-w-7xl mx-auto text-center">
-        <div className="inline-block border border-white px-4 py-1 mb-4 text-sm">
-          CBI
-        </div>
-        <p className="text-xs text-gray-300">
+      <div className="px-[5%] lg:px-[15%] flex flex-col md:flex-row gap-4 md:gap-0 justify-start items-center">
+        <img src={logo} alt="CBI Logo" className="w-[52px]" />
+        <p className="w-full text-base text-white/20 text-center md:pr-[52px]">
           Copyright 2024 CBI. All Rights Reserved
         </p>
       </div>
